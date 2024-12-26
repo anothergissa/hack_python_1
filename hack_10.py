4df11047866a73beec248c5f4b93881724ac6daa
Raziel <razielpadrino2009@gmail.com>
2024-12-25 22:59:45 -0400

    hack resuelto

--- hack_10.py
+++ hack_10.py
@@ -4,5 +4,8 @@ text: "fooziman" output => ["F","0","0","Z","1","M","@","N"]
 
 def fn_hack_10():
     result = "fooziman"
-    #...
-    return result  
+    fooziman = result.upper().replace("O", "0").replace("I", "1").replace("A", "@")
+    result = []
+    for i in fooziman:
+        result.append(i)
+    return result
--- hack_9.py
+++ hack_9.py
@@ -4,5 +4,9 @@ loop: while [1,2,3] ouput => [1,'@',2,'@',3,'@']
 
 def fn_hack_9():
     result = [1,2,3]
-    #...
+    i = 0
+    while i < 6:
+        if i % 2 != 0:
+            result.insert(i, "@")
+        i += 1
     return result  
